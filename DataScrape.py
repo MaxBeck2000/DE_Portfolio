d330e888f2ce4f9142e2b6c9a32efabe0731c0df
Max <maxbeck456@gmail.com>
2024-07-18 14:00:56 +0100

    Finished script

--- DataScrape.py
+++ DataScrape.py
@@ -18,13 +18,33 @@ containers = P_soup.findAll("div", {"class" : "col-12 col-md-6 pr-md-3"})
 
 print(len(containers))
 
-container = containers[0]
-column = ['Country']
-Places = pd.DataFrame(columns = column)
+pattern = r'(\d+)\.\s+([^-\n]+)\s+-\s+(.*)'
+#(\d+): Captures one or more digits.
+#\.: Matches the literal dot after the number.
+#\s+: Matches one or more whitespace characters.
+#([^-\n]+): Captures the country name, which consists of any characters except the dash (-) or newline (\n).
+#\s+-\s+: Matches the dash surrounded by spaces.
+#(.*): Captures the rest of the line, which is the description.
+
+ranks = []
+countries = []
+descriptions = []
 
 for container in containers:
-    p_name = container.findAll("h3"), {"class":"card-heading"}
-    p_num = 
-    print(p_name)
-    # p_nameN = p_name[0].text[4:].strip().split()
-#print(Places.head())
+    p_line = container.find("h3", {"class":"card-heading"})
+    if p_line:
+        text_content = p_line.get_text(strip = True)
+        match = re.search(pattern, text_content)
+
+        if match:
+            rank = match.group(1)
+            country = match.group(2).strip()
+            description = match.group(3).strip()
+            ranks.append(rank)
+            countries.append(country)
+            descriptions.append(description)
+
+country_ranks = pd.DataFrame({'Rank': ranks,
+                              'Country': countries,
+                              'Description': descriptions})
+print(country_ranks)
